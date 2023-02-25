3e1dc58a397ef3a0ba44ad7d5479e37903ae27b6
Omar Casey <omarcasey4@gmail.com>
2023-02-24 21:26:32 -0500

    data change

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -32,26 +32,26 @@ const Home = () => {
             <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
                 <PieChart
                     title="Properties for Sale"
-                    value={684}
-                    series={[75, 25]}
+                    value={503}
+                    series={[55, 45]}
                     colors={["#275be8", "#c4e8ef"]}
                 />
                 <PieChart
                     title="Properties for Rent"
-                    value={550}
+                    value={735}
                     series={[60, 40]}
                     colors={["#275be8", "#c4e8ef"]}
                 />
                 <PieChart
                     title="Total customers"
-                    value={5684}
+                    value={4978}
                     series={[75, 25]}
                     colors={["#275be8", "#c4e8ef"]}
                 />
                 <PieChart
                     title="Properties for Cities"
-                    value={555}
-                    series={[75, 25]}
+                    value={879}
+                    series={[80, 20]}
                     colors={["#275be8", "#c4e8ef"]}
                 />
             </Box>
